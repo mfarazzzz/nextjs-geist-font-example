2d5bf4e7baf5535de5bb4257453630e757a13f8f
mfarazzzz <mfarazzzz@gmail.com>
2025-06-22 14:33:16 +0530

    Add .next/static/chunks/src_app_jobs_page_tsx_63fa8e65._.js 1750582995731

---
+++ .next/static/chunks/src_app_jobs_page_tsx_63fa8e65._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_07e21501._.js"
+],
+    source: "dynamic"
+});
